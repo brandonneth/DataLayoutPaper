1dbc51a917ac52cb36818c121df5ec2804c18e62
Brandon Neth <brandonneth@Brandons-MacBook-Pro.local>
2022-01-28 17:36:11 -0700

    updated to writing based on the evalutaion stufF

--- generate_plots.sh
+++ generate_plots.sh
@@ -5,3 +5,9 @@ cd ScoreValidity
 python3 visualize.py benchmark1.times
 python3 visualize.py benchmark2.times
 python3 visualize.py benchmark3.times
+
+python3 intro-example.py
+
+python3 2mm-all.py
+
+cd ..
