0e41b0c209d0b9114e0c06c9950fe4629a4539c5
Brandon Neth <brandonneth@Brandons-MacBook-Pro.local>
2022-02-02 15:44:50 -0700

    script to move files from submodules to this directory.

---
+++ move_figures.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp ScoreValidity/IntroExampleGraph.pdf .
+cp ScoreValidity/benchmark1_boxplot.pdf .
+cp ScoreValidity/benchmark2_boxplot.pdf .
+cp ScoreValidity/benchmark3_boxplot.pdf .
+
+cp RAJAPerf/PolybenchPerfPlot.pdf .
+cp RAJAPerf/PolybenchSLOC.pdf .
+
+cp ScoreValidity/2mm-all.pdf .
